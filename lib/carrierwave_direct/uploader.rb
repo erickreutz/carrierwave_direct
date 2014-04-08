ba55fdd71acfb35906f4442ac7be571ed796a22d
Eric Kreutzer <eric@airkrft.com>
2014-04-08 15:21:48 -0600

    Don't include file path in policy object

--- lib/carrierwave_direct/uploader.rb
+++ lib/carrierwave_direct/uploader.rb
@@ -35,7 +35,8 @@ module CarrierWaveDirect
       options[:max_file_size] ||= max_file_size
 
       conditions = [
-        ["starts-with", "$utf8", ""]
+        ["starts-with", "$utf8", ""],
+        ["starts-with", "$key", key.split('/').first]
       ]
 
       conditions << ["starts-with", "$Content-Type", ""] if will_include_content_type
